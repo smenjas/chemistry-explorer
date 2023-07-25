584c4241eb5e748deef1d8ede96d8ae1280b81b9
Ben Thornton <smenjas@users.noreply.github.com>
2023-07-25 11:11:00 -0700

    Unwrap the Page class.

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
-import Page from './page.js';
+import Elements from './elements.js';
+import Isotopes from './isotopes.js';
+import Molecules from './molecules.js';
+import Search from './search.js';
+import Test from './test.js';
 
-Page.display();
+/**
+ * Create HTML based on the URL.
+ *
+ * @param {Object} params - URLSearchParams representing the URL's query string
+ * @returns {string} HTML
+ */
+function render(params) {
+    const formula = params.get('formula');
+    const group = parseInt(params.get('group'));
+    const period = parseInt(params.get('period'));
+    const protons = parseInt(params.get('protons'));
+    const search = params.get('search');
+    const view = params.get('view');
+
+    if (formula) {
+        return Molecules.renderFormula(formula);
+    }
+
+    if (group && Elements.groups.has(group)) {
+        document.title = `Group ${group}`;
+        let html = '<main>';
+        html += `<h1>${document.title}</h1>`;
+        html += Elements.renderGroupNav(group);
+        html += Elements.renderGroup(group);
+        html += '</main>';
+        return html;
+    }
+
+    if (period && Elements.periods.has(period)) {
+        document.title = `Period ${period}`;
+        let html = '<main>';
+        html += `<h1>${document.title}</h1>`;
+        html += Elements.renderPeriodNav(period);
+        html += Elements.renderPeriod(period);
+        html += '</main>';
+        return html;
+    }
+
+    if (params.has('search')) {
+        return Search.render(search);
+    }
+
+    switch (view) {
+    case 'abundance':
+        return Elements.renderAbundance();
+    case 'molecules':
+        return Molecules.render();
+    case 'isotopes':
+        return Isotopes.render();
+    case 'test':
+        return Test.render();
+    }
+
+    // Show the periodic table by default.
+    return Elements.render(protons);
+}
+
+const params = new URLSearchParams(window.location.search);
+const html = render(params);
+
+document.body.insertAdjacentHTML('beforeend', html);
+Elements.addEventHandlers();
+Search.addEventHandlers();
