b521892b2f9744e8dcd595f976ef1112f0f490ae
Ben Thornton <smenjas@users.noreply.github.com>
2023-07-31 00:29:13 -0700

    Run nodemon from the project root.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const server = http.createServer((request, response) => {
     case '/':
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/html');
-        content = fs.readFileSync('../client/index.html', 'utf8');
+        content = fs.readFileSync('client/index.html', 'utf8');
         console.log('HTTP', response.statusCode, request.url);
         break;
     case '/index.js':
@@ -39,29 +39,29 @@ const server = http.createServer((request, response) => {
     case '/view/test.js':
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/javascript');
-        content = fs.readFileSync(`../client${path}`, 'utf8');
+        content = fs.readFileSync(`client${path}`, 'utf8');
         break;
     case '/main.css':
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/css');
-        content = fs.readFileSync(`../client${path}`, 'utf8');
+        content = fs.readFileSync(`client${path}`, 'utf8');
         break;
     case '/404.jpg':
         response.statusCode = 200;
         response.setHeader('Content-Type', 'image/jpeg');
-        content = fs.readFileSync(`../client/img${path}`);
+        content = fs.readFileSync(`client/img${path}`);
         break;
     case '/img/icons8-molecule-icongeek26-linear-colour-16.png':
     case '/img/icons8-molecule-icongeek26-linear-colour-32.png':
     case '/img/icons8-molecule-icongeek26-linear-colour-96.png':
         response.statusCode = 200;
         response.setHeader('Content-Type', 'image/png');
-        content = fs.readFileSync(`../client${path}`);
+        content = fs.readFileSync(`client${path}`);
         break;
     default:
         response.statusCode = 404;
         response.setHeader('Content-Type', 'text/html');
-        content = fs.readFileSync('../client/404.html', 'utf8');
+        content = fs.readFileSync('client/404.html', 'utf8');
         console.log('HTTP', response.statusCode, request.url);
         break;
     }
